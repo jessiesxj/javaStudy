9b86bb83ebd5933c9e4970e9a4a936d367c85bdf
jessiesxj <sunxj@outlook.com>
2015-08-16 15:19:13 +0800

    Create ArrayPrint.java

---
+++ ArrayPrint.java
@@ -0,0 +1,35 @@
+/**
+ * Created by jessie on 15-8-14.
+ */
+
+/**
+ * enter 3 integers x,y,z
+ * array them from min to max
+ */
+
+import java.util.Scanner;
+
+public class ArrayPrint {
+
+    public static void main(String[] args){
+        int n;
+        Scanner scanner= new Scanner(System.in);
+        System.out.print("Please enter the value of 'x':");
+        int x=scanner.nextInt();
+        System.out.print("Please enter the value of 'y':");
+        int y=scanner.nextInt();
+        System.out.print("Please enter the value of 'z':");
+        int z=scanner.nextInt();
+        if (x>y){
+            n=x;
+            x=y;
+            y=n;
+        };
+        if (y>z){
+            n=y;
+            y=z;
+            z=y;
+        };
+        System.out.println("arry x,y,z from min to max is:"+" "+x+" "+y+" "+z);
+    }
+}
