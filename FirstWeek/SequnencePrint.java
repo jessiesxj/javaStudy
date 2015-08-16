0c79e0e2dcc8f7ff007efe7e3124653e89ba5237
jessiesxj <sunxj@outlook.com>
2015-08-16 16:53:17 +0800

    Update SequnencePrint.java

--- FirstWeek/SequnencePrint.java
+++ FirstWeek/SequnencePrint.java
@@ -12,28 +12,11 @@
  */
 public class SequnencePrint {
     public static void main(String[] args){
-        for (int i=1;i<=4;i++){
-            if (i==1) {
-                for (int j = 1; j <= 4; j++) {
-                    System.out.print(j + " ");
-                }
-                System.out.println();
-            }
-            else if (i==2){
-                for (int j = 5; j <= 8; j++) {
-                    System.out.print(j + " ");
-                }
-                System.out.println();
-            }
-            else if (i==3){
-                for (int j = 9; j <= 12; j++) {
-                    System.out.print(j + " ");
-                }
-                System.out.println();
-            }
-            else if (i==4){
-                for (int j = 13; j <= 16; j++) {
-                    System.out.print(j + " ");
+        for (int i=0;i<4;i++){
+           for(int j=1;j<=4;j++){
+               int num=i*4+j;
+               Systerm.out.print(num);
+           }
                 }
                 System.out.println();
             }
