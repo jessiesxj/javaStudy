3cef6b19e16e245534e246c028b316a3e45c81aa
jessiesxj <sunxj@outlook.com>
2015-08-16 16:37:26 +0800

    Update MultiplicationFormulas.java

--- FirstWeek/MultiplicationFormulas.java
+++ FirstWeek/MultiplicationFormulas.java
@@ -14,10 +14,9 @@
 
 public class MultiplicationFormulas {
     public static void main(String[] args){
-        int i,j,x;
-        for(i=1;i<=9;i++){
-            for(j=1;j<=i;j++){
-                x=i*j;
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=i;j++){
+                int x=i*j;
                 System.out.print(j + "x" + i + "=" + x+" ");
             }
             System.out.println();
