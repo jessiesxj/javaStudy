6f0f8ff828bcd14dde9d7669ec47951c02eac475
jessiesxj <sunxj@outlook.com>
2015-08-16 15:20:47 +0800

    Create MultiplicationFormulas.java

---
+++ MultiplicationFormulas.java
@@ -0,0 +1,27 @@
+/**
+ * Created by jessie on 15-8-15.
+ */
+
+/**
+ * multiplication
+ * 1x1=1
+ * 1x2=2
+ * .....
+ * 9x9=81
+ *
+ */
+
+
+public class MultiplicationFormulas {
+    public static void main(String[] args){
+        int i,j,x;
+        for(i=1;i<=9;i++){
+            for(j=1;j<=i;j++){
+                x=i*j;
+                System.out.print(j + "x" + i + "=" + x+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
